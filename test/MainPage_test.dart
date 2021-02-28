df3dabb5c49fce49878715e1850c86751de35cba
Evgeny Varov <evsxez@gmail.com>
2021-03-01 01:56:33 +0700

    red

--- test/MainPage_test.dart
+++ test/MainPage_test.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('has bottom navigation bar', (WidgetTester tester) async {
-    await tester.pumpWidget(MainPage());
-    expect(find.byType(BottomNavigationBar), findsOneWidget);
+  group('navigation bar', () {
+    testWidgets('has bottom navigation bar', (WidgetTester tester) async {
+      await tester.pumpWidget(MainPage());
+      expect(find.byType(BottomNavigationBar), findsOneWidget);
+    });
+
+    testWidgets('has 4 pages', (WidgetTester tester) async {
+      await tester.pumpWidget(MainPage());
+      expect(find.byType(BottomNavigationBarItem), findsNWidgets(4));
+    });
+
   });
 
 
