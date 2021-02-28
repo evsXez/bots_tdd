8539cd8731f2450fe31d568b4c60dbf8d897fe54
Evgeny Varov <evsxez@gmail.com>
2021-03-01 02:12:11 +0700

    red

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -1,21 +1,45 @@
+// import 'package:bots_no_tdd/data/User.dart';
 import 'package:bots_tdd/resources/Strings.dart';
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+import 'CreatePage.dart';
+// import 'ReadPage.dart';
+// import 'StreamPage.dart';
+// import 'UpdatePage.dart';
+
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+
+class _MainPageState extends State<MainPage> {
+
+  static const int PAGE_CREATE = 0;
+  static const int PAGE_READ = 1;
+  static const int PAGE_UPDATE = 2;
+  static const int PAGE_STREAM = 3;
+
+  int currentPageIdx = 0;
+  // User userToUpdate;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        body: SafeArea(child: page),
         bottomNavigationBar: bottomNavigation,
       ),
     );
   }
 
   Widget get bottomNavigation => BottomNavigationBar(
-    // onTap: pageSelected,
+    onTap: pageSelected,
     fixedColor: Colors.deepOrange,
     unselectedItemColor: Colors.blue,
-    // currentIndex: currentPageIdx,
+    currentIndex: currentPageIdx,
     items: [
       BottomNavigationBarItem(icon: Icon(Icons.add), label: Strings.label_create),
       BottomNavigationBarItem(icon: Icon(Icons.list), label: Strings.label_read),
@@ -24,4 +48,27 @@ class MainPage extends StatelessWidget {
     ]
   );
 
+  void pageSelected(int idx) { setState(() { currentPageIdx = idx; }); }
+
+  Widget get page {
+    switch (currentPageIdx) {
+      case PAGE_CREATE: return CreatePage();
+      // case PAGE_READ: return ReadPage(onUserUpdateRequest);
+      // case PAGE_UPDATE: return UpdatePage(user: userToUpdate, updated: resetUser);
+      // case PAGE_STREAM: return StreamPage();
+    }
+    return Container(color: Colors.orange,);
+  }
+
+  // void onUserUpdateRequest(User user) {
+  //   setState(() { 
+  //     userToUpdate = user;
+  //     currentPageIdx = PAGE_UPDATE; 
+  //   });
+  // }
+
+  // void resetUser() { setState(() { userToUpdate = null; }); }
+
+  
 }
+
--- test/MainPage_test.dart
+++ test/MainPage_test.dart
@@ -1,17 +1,35 @@
+import 'package:bots_tdd/pages/CreatePage.dart';
 import 'package:bots_tdd/pages/MainPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+
+
+
   group('navigation bar', () {
+
     testWidgets('has bottom navigation bar', (WidgetTester tester) async {
       await tester.pumpWidget(MainPage());
       expect(find.byType(BottomNavigationBar), findsOneWidget);
     });
 
-    testWidgets('has 4 pages', (WidgetTester tester) async {
+    testWidgets('has 4 page icons', (WidgetTester tester) async {
       await tester.pumpWidget(MainPage());
-      expect(find.byType(BottomNavigationBarItem), findsNWidgets(4));
+      expect(find.byType(Icon), findsNWidgets(4));
+    });
+
+    testWidgets('initially shows CreatePage', (WidgetTester tester) async {
+      await tester.pumpWidget(MainPage());
+      expect(find.byType(CreatePage), findsOneWidget);
+    });
+
+    testWidgets('tap on second button shows ReadPage', (WidgetTester tester) async {
+      await tester.pumpWidget(MainPage());
+
+      await tester.tap(find.byIcon(Icons.list));
+      expect(find.byType(ReadPage), findsOneWidget);
+      expect(find.byType(CreatePage), findsNothing);
     });
 
   });
