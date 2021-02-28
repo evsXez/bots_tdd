231235494f3fa25eeef4fd2fb7f73949a5ac5596
Evgeny Varov <evsxez@gmail.com>
2021-03-01 01:53:42 +0700

    green

---
+++ lib/pages/MainPage.dart
@@ -0,0 +1,27 @@
+import 'package:bots_tdd/resources/Strings.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        bottomNavigationBar: bottomNavigation,
+      ),
+    );
+  }
+
+  Widget get bottomNavigation => BottomNavigationBar(
+    // onTap: pageSelected,
+    fixedColor: Colors.deepOrange,
+    unselectedItemColor: Colors.blue,
+    // currentIndex: currentPageIdx,
+    items: [
+      BottomNavigationBarItem(icon: Icon(Icons.add), label: Strings.label_create),
+      BottomNavigationBarItem(icon: Icon(Icons.list), label: Strings.label_read),
+      // BottomNavigationBarItem(icon: Icon(Icons.edit), label: Strings.label_update),
+      // BottomNavigationBarItem(icon: Icon(Icons.arrow_circle_down), label: Strings.label_stream),
+    ]
+  );
+
+}
---
+++ lib/resources/Strings.dart
@@ -0,0 +1,25 @@
+class Strings {
+  
+  static final label_create = "CREATE";
+  static final label_read = "READ";
+  static final label_update = "UPDATE";
+  static final label_delete = "DELETE";
+  static final label_cancel = "CANCEL";
+  static final label_stream = "Stream";
+
+  static final error_network = "Network error";
+  
+  static final hint_name = "Enter your name";
+  static final hint_comment = "Add comment if you wish";
+  static final hint_press_long_for_menu = "Press long ;)";
+  static final hint_select_user = "Select user from the list first";
+  static final hint_add_something = "Add something";
+
+  static String hello;
+  
+  static String anonym = "Mr. Anonym";
+  static String ok = "OK";
+  static String close = "Close";
+  static String end_of_list = "End of list.";
+
+}
